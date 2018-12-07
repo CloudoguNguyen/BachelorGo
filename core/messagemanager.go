de835ca152b8e6d6709c08ecbcd8ad7977a519b7
CloudoguNguyen <33620644+CloudoguNguyen@users.noreply.github.com>
2018-12-07 10:57:54 +0100

    refactor

--- core/messagemanager.go
+++ core/messagemanager.go
@@ -36,30 +36,28 @@ func (manager *MessageManager) Response(message string, conversationID string) (
 		return "", errors.Wrapf(err, "failed to add message to json with %s", conversationID)
 	}
 
-	profile, err := manager.getUserProfile(&message, path)
+	profile, err := manager.getUserProfile(path)
 	if err != nil {
-		return "", errors.Wrapf(err, "failed to update profile in conversation %s", conversationID)
+		if strings.Contains(err.Error(), "less than the minimum number of words required") {
+			message = profile_not_valid
+		} else {
+			return "", errors.Wrapf(err, "failed to update profile in conversation %s", conversationID)
+		}
 	}
 
 	answer, err := manager.responder.GetResponse(message, conversationID, &profile)
 	if err != nil {
 		return "", errors.Wrapf(err, "failed to get reply with the message %s", message)
 	}
-
 	return answer, nil
 }
 
-func (manager *MessageManager) getUserProfile(message *string, path string) (service.UserProfile, error) {
+func (manager *MessageManager) getUserProfile(path string) (service.UserProfile, error) {
 
 	profile, err := manager.watsonPI.GetUserProfile(path)
 	if err != nil {
-		if strings.Contains(err.Error(), "less than the minimum number of words required") {
-			*message = profile_not_valid
-		} else {
-			return profile, errors.Wrapf(err, "failed update profile in conversation")
-		}
+		return service.UserProfile{}, errors.Wrapf(err, "failed update profile in conversation")
 	}
-
 	return profile, nil
 }
 
